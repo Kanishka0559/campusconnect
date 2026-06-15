7fe78f2c7cea68e4039301eeb905912e630903c4
Kanishka Shree S <kanishkashree47@gmail.com>
2026-06-15 22:11:52 +0530

    Created React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>DevConnect</h1>
+      <p>Student Networking Platform</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <h1>Welcome to DevConnect</h1>
+      <p>Connect. Learn. Grow.</p>
+    </>
+  );
+}
+
+export default App;
